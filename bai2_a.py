0a776f164d554f4ff4b2a0b10849ab91a44b5a6c
Linh Quang Nguyễn <90489380+linhnq2000@users.noreply.github.com>
2021-09-25 11:15:42 +0700

    Update bai2_a.py

--- bai2_a.py
+++ bai2_a.py
@@ -1,7 +1,5 @@
-a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 88]
-b = [1, 3, 5, 4, 7, 88, 66, 59, 40, 54]
-c=[]
-for i in max(a,b):
-    if i in min(a, b) and i not in c:
-            c.append(i) 
-    print(c)
+A = [1,1,2,3,5,8,13,21,34,55,88]
+B = [1,3,5,4,7,88,66,59,40,54]
+print(set(A) & set(B))
+C= set(A) & set(B)
+print(C)
